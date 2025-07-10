0d5005724899e0cce882bead8d04b3dccda032a9
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-10 10:12:47 +0400

    hash password before create

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -16,6 +17,14 @@ const userSchema = new mongoose.Schema({
         required: true,
         minlength: 6
     }
+});
+
+// Middleware to hash password before saving
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
 });
 
 module.exports = mongoose.model('User', userSchema);
