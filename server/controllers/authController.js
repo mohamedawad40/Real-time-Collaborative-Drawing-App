18ec4f07954ea9197619fad2de83d02c700e84ec
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-10 10:13:16 +0400

    create token after signup

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,12 @@
 const AppError = require("../utils.js/appError");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+
+const signToken = function (id) {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+};
 
 const register = async (req, res, next) => {
     try {
@@ -17,7 +24,8 @@ const register = async (req, res, next) => {
             password
         });
         await newUser.save();
-        return res.status(201).json({ message: 'User registered successfully' });
+        const token = signToken(newUser._id);
+        return res.status(201).json({ status: 'success', token, message: 'User registered successfully' });
     } catch (error) {
         console.error('Error registering user:', error);
         return next(new AppError('Error registering user', 500));
