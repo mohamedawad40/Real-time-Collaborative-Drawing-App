a317cb692586adf7a2fc3837defe94918eced4bc
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-10 15:30:28 +0400

    update routes

--- server/routes/roomRoute.js
+++ server/routes/roomRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { createRoom, joinRoom } = require('../controllers/roomController');
+const { createRoom, joinRoom, getRoomById, getAllPublicRooms } = require('../controllers/roomController');
 const { protectRoute } = require('../controllers/authController');
 const router = express.Router();
 
 router.route('/').post(protectRoute, createRoom);
 router.route('/:roomId').post(protectRoute, joinRoom);
+router.route('/public').get(protectRoute, getAllPublicRooms);
+router.route('/:id').get(protectRoute, getRoomById);
 
 
 module.exports = router;
