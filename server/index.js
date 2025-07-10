4504585f5f2f947a6e621088e2cf2363ba70ecbc
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-10 10:24:11 +0400

    add user route

--- server/index.js
+++ server/index.js
@@ -3,14 +3,21 @@ const http = require('http');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const errorHandler = require('./utils.js/errorHandling');
 dotenv.config();
 
+const userRoute = require('./routes/userRoute');
 const app = express();
 const server = http.createServer(app);
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use('/api/user', userRoute);
+// Global error handler
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
