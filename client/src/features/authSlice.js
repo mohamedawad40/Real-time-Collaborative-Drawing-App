5a793d26b20b28007867924c2215d0a5d0b355fb
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-11 14:04:20 +0400

    add auth slice

---
+++ client/src/features/authSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { register } from '../app/hooks';
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+const initialState = {
+    user: user || null,
+    isLoading: false,
+    isSuccess: false,
+    isError: false,
+    message: '',
+};
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.user = action.payload;
+            })
+            .addCase(register.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+                state.user = null;
+            })
+
+    }
+})
+
+export default authSlice.reducer;
