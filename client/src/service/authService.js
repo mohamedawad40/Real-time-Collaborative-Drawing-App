56182408fd08feeb8ce3c0febb40e20e80c60f04
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-11 14:24:16 +0400

    add login functions

--- client/src/service/authService.js
+++ client/src/service/authService.js
@@ -1,4 +1,11 @@
-import axios from '../../utils/axios';
+
+const login = async (userData) => {
+    const res = await axios.post('/api/user/login', userData);
+    if (res.data) {
+        localStorage.setItem('user', JSON.stringify(res.data));
+    }
+    return res.data;
+}
 
 const register = async (userData) => {
     const res = await axios.post('/api/user/signup', userData);
@@ -8,6 +15,12 @@ const register = async (userData) => {
     return res.data;
 }
 
+const logout = () => {
+    localStorage.removeItem('user');
+};
+
 export const authService = {
-    register
+    register,
+    login,
+    logout
 }
