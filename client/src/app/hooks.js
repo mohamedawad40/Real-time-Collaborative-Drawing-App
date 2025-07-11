ec9eae1ae41bb4785eba9a556f8b49f14859e68f
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-11 14:25:37 +0400

    add hooks

--- client/src/app/hooks.js
+++ client/src/app/hooks.js
@@ -13,3 +13,17 @@ export const register = createAsyncThunk('/user/register', async (userData, { re
     }
 })
 
+export const login = createAsyncThunk('/user/login', async (userData, { rejectWithValue }) => {
+    try {
+        const user = await authService.login(userData);
+        console.log('user:', user);
+        return user
+    } catch (error) {
+        const message = (error.response && error.response.data && error.response.data.message) || error.message || 'Login failed';
+        return rejectWithValue(message);
+    }
+})
+
+export const logout = createAsyncThunk('/user/logout', async () => {
+    await authService.logout();
+});
