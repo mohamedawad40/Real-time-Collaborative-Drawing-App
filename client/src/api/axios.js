fa2547b266f5e595710f2cb115f994a934110dbe
mohamedawad40 <mohamed.awad.elgammal@gmail.com>
2025-07-13 13:08:54 +0400

    use interceptors

---
+++ client/src/api/axios.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+
+const API = axios.create({
+    baseURL: 'http://localhost:3000/api',
+    withCredentials: true,
+})
+
+
+// 🔐 Interceptor: Attach token
+API.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+        if (token) {
+        config.headers["Authorization"] = `Bearer ${token}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+API.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        const status = error.response?.status;
+        if (status === 401) {
+            console.warn("Unauthorized. Redirecting to login...");
+        }
+        return Promise.reject(error);
+    }
+);
+export default API;
